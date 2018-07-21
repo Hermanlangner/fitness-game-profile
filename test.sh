d9dd2468c3df7a67ce5f96acef97b96911646c3f
Herman Langner <hermandlangner@gmail.com>
2018-07-21 04:35:05 +0200

    Changed to an accepted format

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 dotnet restore ./Fitness.Game.Profile.UnitTests/Fitness.Game.Profile.UnitTests.csproj
 
-dotnet test ./Fitness.Game.Profile.UnitTests/Fitness.Game.Profile.UnitTests.csproj /p:CollectCoverage=true
+dotnet test ./Fitness.Game.Profile.UnitTests/Fitness.Game.Profile.UnitTests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
 
 curl -s https://codecov.io/bash > codecov
 chmod +x codecov
-sudo bash ./codecov -f ./Fitness.Game.Profile.UnitTests/coverage.json -t 0152bd84-c56e-4017-a16e-eb788a28f82f
+./codecov -f ./Fitness.Game.Profile.UnitTests/coverage.opencover.xml
