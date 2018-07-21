cc8b617c8095a4531d155a6985267df786bdef49
Herman Langner <hermandlangner@gmail.com>
2018-07-21 04:23:23 +0200

    upped permissions for code cov

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ dotnet test ./Fitness.Game.Profile.UnitTests/Fitness.Game.Profile.UnitTests.cspr
 
 curl -s https://codecov.io/bash > codecov
 chmod +x codecov
-./codecov -f ./Fitness.Game.Profile.UnitTests/coverage.json
+sudo bash ./codecov -f ./Fitness.Game.Profile.UnitTests/coverage.json -t 0152bd84-c56e-4017-a16e-eb788a28f82f
