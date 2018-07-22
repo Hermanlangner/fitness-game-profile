5348f15e4822fd9db38e414532da2177c6362c61
Hermanlangner <hermandlangner@gmail.com>
2018-07-22 11:05:07 +0200

    SonarQube ci  try1 (#3)
    * SonarQube ci  try1
    
    * Turns out I need a dorra sign
    
    * Updated pathing
    
    * Added debugging
    
    * Changed more pathing
    
    * More tweaks
    
    * Interesting

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 dotnet restore ./Fitness.Game.Profile.UnitTests/Fitness.Game.Profile.UnitTests.csproj
 
-dotnet test ./Fitness.Game.Profile.UnitTests/Fitness.Game.Profile.UnitTests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
 
+dotnet test ./Fitness.Game.Profile.UnitTests/Fitness.Game.Profile.UnitTests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=\"opencover,lcov\"
 curl -s https://codecov.io/bash > codecov
 chmod +x codecov
 ./codecov -f ./Fitness.Game.Profile.UnitTests/coverage.opencover.xml
